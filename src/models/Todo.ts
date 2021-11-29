863fc4af83271c5bf512794ec91723780d889e4e
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-29 16:23:11 -0300

    chore: create Todo model

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,5 +1,18 @@
+import { v4 as uuid } from 'uuid'
+
 class Todo {
-  id: string;
+  id?: string;
+  content: string;
+  isComplete: boolean;
+  created_at: Date;
+  updated_at?: Date;
+
+
+  constructor(){
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
 export { Todo };
