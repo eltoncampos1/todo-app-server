72245e7449087b8f847ed70d99314c8c12229cc0
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-29 18:05:09 -0300

    chore: list todos route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
-import { createTodoController } from '../use-cases';
-import { Router } from 'express'
+import { createTodoController, listTodoController } from '../use-cases';
+import { request, response, Router } from 'express'
 
 const todoRoutes = Router();
 
 
+todoRoutes.get('/list', (request, response) => listTodoController.handle(request, response))
 todoRoutes.post('/create',(request, response) => createTodoController.handle(request, response))
 
 export { todoRoutes }
