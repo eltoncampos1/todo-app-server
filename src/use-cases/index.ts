ccb2e9498cd16b6fec52f09ab25a6eee6d10e133
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-29 18:04:55 -0300

    chore: list todos controller

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -1,11 +1,13 @@
 import { TodoRepository } from "implementations/todo";
-import { CreateTodoUseCase } from "./create-todo";
-import { CreateTodoController } from "./create-todo-controller";
+import { CreateTodoController, CreateTodoUseCase } from "./create-todo";
+import { ListTodoController, ListTodoUseCase } from "./list-todo";
 
 const todoRepository = new TodoRepository()
 const createTodoUseCase = new CreateTodoUseCase(todoRepository)
+const listTodoUseCase = new ListTodoUseCase(todoRepository)
+const listTodoController = new ListTodoController(listTodoUseCase)
 
 const createTodoController = new CreateTodoController(createTodoUseCase)
 
 
-export { createTodoController }
+export { createTodoController, listTodoController }
