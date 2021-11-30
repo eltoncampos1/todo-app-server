6acd0b8f36d8ca3c726e0a2ea3f94c3eff6f23d6
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-30 19:53:18 -0300

    chore: add findById contract

--- src/repositories/todo.ts
+++ src/repositories/todo.ts
@@ -1,11 +1,10 @@
 import { ICreateTodoDTO, IDeleteTodoDTO, IUpdateTodoDTO } from "../DTOs";
 import { Todo } from "src/entities/Todo"
 
-
-
 interface ITodoRepository {
     create(data: ICreateTodoDTO): Promise<void> 
     list(): Promise<Todo[]>
+    findById(id: string): Promise<Todo | undefined>
     update(data: IUpdateTodoDTO): Promise<void>
     delete:(data: IDeleteTodoDTO) => Promise<void>
 }
