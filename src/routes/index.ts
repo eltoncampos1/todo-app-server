f847658333a4614acd30f781a91723202b1945bb
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-29 22:08:51 -0300

    chore: delete todo route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { createTodoController, listTodoController, updateTodoController } from '../use-cases';
+import { createTodoController, deleteTodoController, listTodoController, updateTodoController } from '../use-cases';
 import { request, response, Router } from 'express'
 
 const todoRoutes = Router();
@@ -7,5 +7,6 @@ const todoRoutes = Router();
 todoRoutes.get('/list', (request, response) => listTodoController.handle(request, response))
 todoRoutes.post('/create',(request, response) => createTodoController.handle(request, response))
 todoRoutes.patch('/update/:todoId', (request, response) => updateTodoController.handle(request, response))
+todoRoutes.delete('/delete/:todoId',(request, response) => deleteTodoController.handle(request, response))
 
 export { todoRoutes }
