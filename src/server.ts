05143bddfca8a8ec59b4ee56fd0f9043ef6b0237
eltoncampos1 <eltoncampos36@gmail.com>
2021-11-29 22:24:54 -0300

    feat: create error handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import { AppError } from "errors/AppError";
+import express, {Request, Response, NextFunction} from "express";
 import { todoRoutes } from "./routes";
 
 const app = express();
 app.use(express.json())
 app.use(todoRoutes)
+
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 app.listen(3333, () => console.log("App is Running"));
